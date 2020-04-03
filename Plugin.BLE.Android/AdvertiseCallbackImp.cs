4e622dc598e024e8234053eb08b053409ce97dc8
germang13 <german.gar13@gmail.com>
2020-04-03 15:38:14 -0300

    Subo Plugin agregando la funcionalidada para hacer advertisement

---
+++ Plugin.BLE.Android/AdvertiseCallbackImp.cs
@@ -0,0 +1,19 @@
+﻿using Android.Bluetooth.LE;
+using Android.Runtime;
+
+namespace Plugin.BLE.Android
+{
+    internal class AdvertiseCallbackImp : AdvertiseCallback
+    {
+
+        public override void OnStartFailure([GeneratedEnum] AdvertiseFailure errorCode)
+        {
+            base.OnStartFailure(errorCode);
+        }
+
+        public override void OnStartSuccess(AdvertiseSettings settingsInEffect)
+        {
+            base.OnStartSuccess(settingsInEffect);
+        }
+    }
+}
